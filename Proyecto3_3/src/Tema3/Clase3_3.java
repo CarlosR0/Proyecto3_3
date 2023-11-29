bf7998d8f79c244aa97ef2ec219aac98c4f59578
CarlosR <crodriguez5153@ceuandalucia.es>
2023-11-29 17:41:41 +0100

    Cambio clase3_3

--- Proyecto3_3/src/Tema3/Clase3_3.java
+++ Proyecto3_3/src/Tema3/Clase3_3.java
@@ -3,7 +3,7 @@ package Tema3;
 public class Clase3_3 {
 
 	public static void main(String[] args) {
-		System.out.println("Practica 3_3 Carlos Rodríguez Cuéllar");
+		System.out.println("Canela en Rama");
 
 	}
 
